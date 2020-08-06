1cac8d922426e57243def1aab085dc8382a240ca
danielprast <danielprastiwa@gmail.com>
2020-08-06 16:20:18 +0700

    inital Commit

---
+++ Thirty Days Of Code/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Thirty Days Of Code
+//
+//  Created by Daniel Prastiwa on 06/08/20.
+//  Copyright © 2020 Danielprast. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
